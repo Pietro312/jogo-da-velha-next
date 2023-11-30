5b4b4cf826d2f9898e4c3988bdb8f1b7836e94fb
Vitor Rodrigues <150471014+Pietro312@users.noreply.github.com>
2023-11-30 14:04:08 -0300

    melhorando

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return  
+  return (
+    <div>
+      <h1>Jogo da Velha</h1>
+      <title>
+        Jogo da velha
+      </title>
+      <table>
+        <tr>
+          <td>X</td>
+          <td>X</td>
+          <td>X</td>
+        </tr> 
+        <tr>
+          <td>X</td>
+          <td>X</td>
+          <td>X</td>
+        </tr>
+        <tr>
+          <td>X</td>
+          <td>X</td>
+          <td>X</td>
+        </tr>
+      </table>
+    </div>
+
+
+  )
 }

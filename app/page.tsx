ac883213abc1a2da2520c1cfa4fc7902ced7c9eb
Pietro Peixe Junkes <pipejunkes@gmail.com>
2023-11-30 17:11:58 -0300

    jogo da velha com contabilizador

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
-let Pontos = 0
+"use client"
 export default function Home() {
+  let pontos = 0
+  function add () {
+    pontos = pontos+1 
+    let count = document.querySelector("#spanum")
+    if(count) count.innerHTML = pontos.toString()
+    console.log (pontos)
+  }
+  
   return (
     <div>
       <h1>Jogo da Velha</h1>
-    
       <table>
         <tbody>
-
           <tr>
             <td></td>
             <td></td>
@@ -26,9 +32,12 @@ export default function Home() {
       <button id="um" >
         <a id="aum" href="/">Reiniciar</a>
       </button>
-      <button id="dois">
+      <button id="dois" onClick={add}>
         +1 ponto
       </button>
+      <span id="spanum" className="text-white">
+        {pontos}
+      </span>
 
     
     
